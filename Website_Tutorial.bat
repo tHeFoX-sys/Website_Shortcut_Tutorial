22242480b71aca8f32645891e23b0c49bb7f73dd
THe_FøX <aristeidisstagkos21203@gmail.com>
2026-05-21 22:23:29 +0300

    Add files via upload

---
+++ Website_Tutorial.bat
@@ -0,0 +1,13 @@
+@echo off
+title website shortcut tutorial
+color 2
+echo This is a tutorial on how to make a shortcut using batchscripting
+echo It's easier than it looks
+pause
+echo You have to add first the executable of the browser of your choosing
+echo Afterwards you just have to add the URL of the website you want to visit
+echo For this tutorial I use TikTok and Chrome but you can change that as well
+echo Bare in mind that in order to use this as a shortcut you must also save the bat file in your home screen
+start chrome.exe www.tiktok.com
+pause
+exit
